0074eb7177c9063802a801042f9b81306a45f3bd
palomadrc <20253ds130@utez.edu.mx>
2026-01-08 23:16:25 -0600

    Proyecto 01:hola mundo

---
+++ proyecto-01-hola-mundo/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nombre: Paloma Del Río");
+        System.out.println("Grupo: 2E");
+        System.out.println("Fecha: 08/01/2026");
+        System.out.println("Hola mundo");
+    }
+}
