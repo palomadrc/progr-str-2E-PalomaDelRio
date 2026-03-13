1272c6b80076d4e1d5abcf89975fa3934d44c895
palomadrc <20253ds130@utez.edu.mx>
2026-03-12 20:48:16 -0600

    ListView add age

---
+++ DemoListView1/src/main/java/com/example/demolistview/controllers/AppControllers.java
@@ -0,0 +1,106 @@
+package com.example.demolistview.controllers;
+
+import com.example.demolistview.services.PersonService;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.util.List;
+
+public class AppControllers {
+
+    @FXML
+    private Label lblMsg;
+    @FXML
+    private ListView<String> listView;
+    @FXML
+    private TextField txtName;
+    @FXML
+    private TextField txtEmail;
+    @FXML
+    private TextField txtEdad;
+
+
+     //conersion del listado a ObservableList
+    @FXML
+    private ObservableList<String> data= FXCollections.observableArrayList();
+
+    private PersonService service=  new PersonService();
+
+    @FXML
+    public void initialize(){
+        loadFromFile();
+        listView.setItems(data);
+    }
+
+    @FXML
+    public void onReload(){
+        loadFromFile();
+    }
+
+    @FXML
+    public void onAdd(){
+        try {
+            String name=txtName.getText();
+            String email=txtEmail.getText();
+            String edadTexto=txtEdad.getText();
+
+            int edad = Integer.parseInt(edadTexto);
+            if (edad <= 18) {
+                lblMsg.setText("La edad debe ser mayor a 18");
+                lblMsg.setStyle("-fx-text-fill: #f5405e");
+                return;
+            }
+
+
+
+            service.addPerson(name, email, edad);
+            lblMsg.setText("Persona creada con éxito");
+            lblMsg.setStyle("-fx-text-fill: purple");
+
+            txtEmail.clear();
+            txtName.clear();
+            txtEdad.clear();
+
+            loadFromFile();
+            }catch (NumberFormatException e){
+                lblMsg.setText("Edad inválida");
+                lblMsg.setStyle("-fx-text-fill: blue");
+            }catch (IOException e){
+                lblMsg.setText("Hubo un error con el archivo: ");
+                lblMsg.setStyle("-fx-text-fill: blue");
+            }catch (IllegalArgumentException e){
+                lblMsg.setText("Hubo un error con los datos"+e.getMessage());
+                lblMsg.setStyle("-fx-text-fill: blue");
+        }
+
+    }
+
+
+    private void loadFromFile(){
+        try{
+            List<String> items= service.loadDataListView();
+            data.setAll(items);
+            lblMsg.setText("Datos cargados con exito");
+            lblMsg.setStyle("-fx-text-fill: green");
+        }catch (IOException e){
+            lblMsg.setText("Error al cargar los archivos: "+e.getMessage());
+            lblMsg.setStyle("-fx-text-fill: red");
+        }
+
+    }
+
+
+    public TextField getTxtEdad() {
+        return txtEdad;
+    }
+
+    public void setTxtEdad(TextField txtEdad) {
+        this.txtEdad = txtEdad;
+    }
+}
---
+++ DemoListView1/src/main/java/com/example/demolistview/repository/PersonFileRepository.java
@@ -0,0 +1,30 @@
+package com.example.demolistview.repository;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+public class PersonFileRepository {
+
+    private final Path filepath = Paths.get("data", "persosn csv");
+
+    private void ensureFile() throws IOException {
+        if (Files.notExists(filepath)){
+            Files.createFile(filepath);
+        }
+    }
+    public List<String> readAllLines() throws IOException {
+        ensureFile();
+        return Files.readAllLines(filepath);
+    }
+
+    public void appendNewLine(String line) throws IOException {
+        ensureFile();
+        Files.writeString(filepath,line+System.lineSeparator(), StandardCharsets.UTF_8,
+        StandardOpenOption.APPEND);
+    }
+}
